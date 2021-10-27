2c6d1fb356236fc5911b28e5371311f2e2d79f52
Rod Boev <rod.boev@gmail.com>
2021-10-27 13:37:24 -0400

    Add category transformation POC

---
+++ categories.js
@@ -0,0 +1,180 @@
+const csv = require('csvtojson');
+let categories;
+const yelp = require('./categories.json');
+
+function findParentByAlias(alias) {
+  const cat = yelp.find(o => o.alias === alias);
+  const parent = cat.parents;
+  const title = cat.title;
+	
+  if (parent.length === 0) return alias;
+  if (categories.find(o => o.category === title)) return alias;
+
+  // console.log(`${cat} --> ${parent} (parent)`);
+  return findParentByAlias(parent[0]);
+}
+
+function findName(alias) {
+  return yelp.find(o => o.alias === alias).title;
+}
+
+function matchCategory(cat) {
+  const categoryInList = categories.find(o => o.category === cat);
+  if (categoryInList) {
+    // console.log(`${cat} --> OK`);
+    return categoryInList.category;
+  }
+  else {
+    const alias = yelp.find(o => o.title === cat).alias;
+    const parentAlias = findParentByAlias(alias);
+    const parent = findName(parentAlias);
+    // console.log(`${cat} --> ${parentAlias} (parent) --> ${parent}`);
+    if (categories.find(o => o.category === parent)) {
+      return parent;
+    }
+    else {
+      matchCategory(parent);
+    }
+  }
+}
+
+function matchCategories(cats) {
+  let matched = [];
+  for (cat of cats) {
+    matched.push(matchCategory(cat));
+  }
+  return matched;
+}
+
+function renameCategory(cat) {
+  return categories.find(o => o.category === cat).newCategory;
+}
+
+function renameCategories(cats) {
+  let renamed = [];
+  for (cat of cats) {
+    renamed.push(renameCategory(cat));
+    // console.log(`Adding ${renameCategory(cat)} to category list`);
+  }
+  return renamed;
+}
+
+function getWeights(cats) {
+  let weights = [];
+  for (cat of cats) {
+    const weight = categories.map(o => o.newCategory).includes(cat) ?
+      Number(categories.find(o => o.newCategory === cat).weight) :
+      1;
+
+    const categoryExists = weights.find(o => o.category === cat);
+    if (categoryExists && categoryExists.weight) {
+      categoryExists.weight += weight;
+    }
+    else {
+      weights.push({
+        category: cat,
+        weight: weight
+      });
+    }
+  }
+  return weights;
+}
+
+function getHeaviest(weights) {
+  const sorted = weights.sort(function(a, b) { return b.weight-a.weight });
+  const max = sorted[0].weight;
+  const atMax = sorted.filter(o => o.weight === max);
+  const last = atMax[atMax.length-1];
+  const first = atMax[0];
+  if (atMax.length > 1) {
+    const cats = atMax.map(o => o.category);
+    console.log(`[WARNING] Equal weights (${max}): ${cats.join(', ')}`);
+    console.log(`[WARNING] Using ${first.category}`);
+  }
+  return first.category;
+}
+
+function getCategories(str) {
+  if (str.includes(',')) {
+    const cats = str.split(',').map(s => s.trim());
+    return cats;
+  }
+  else {
+    return [str];
+  }
+}
+
+const pipeline = (...fns) => fns.reduce((f, g) => (...args) => g(f(...args)));
+
+(async () => {
+  categories = await csv().fromFile('./catmap.csv')
+  const locations = [
+    /* {
+      name: 'Tryon Public House',
+      categories: 'Bars, Gastropubs, American (Traditional)',
+    },
+    {
+      name: 'Bronx Alehouse',
+      categories: 'Pubs, American (Traditional)',
+    },
+    {
+      name: 'Accent Reduction and ESL for Business',
+      categories: 'Private Tutors, Adult Education, Language Schools',
+    }, */
+    {
+      name: 'Columbia Ob/Gyn Uptown',
+      categories: 'Medical Centers, Obstetricians & Gynecologists, Adult Education'
+    },
+    {
+      name: 'Glam House Bx',
+      categories: 'Party & Event Planning, Venues & Event Spaces, Party Equipment Rentals'
+    },
+    {
+      name: 'Lincoln Technical Institute - Queens',
+      categories: 'Colleges & Universities, Adult Education, Vocational & Technical School'
+    },
+    {
+      name: 'Samuel Field Y',
+      categories: 'Preschools, Community Service/Non-Profit, Child Care & Day Care'
+    },
+    {
+      name: 'Bohemian Hall & Beer Garden',
+      categories: 'Beer Gardens, Venues & Event Spaces',
+    },
+    {
+      name: 'Hotel 64',
+      categories: 'Hotels',
+    },
+    {
+      name: 'Threes Brewing',
+      categories: 'Beer Bar, Burgers, Venues & Event Spaces',
+    },
+    {
+      name: 'Today S Learning Center',
+      categories: '8351-00 Child day care services',
+    },
+  ]
+  console.log();
+  for (location of locations) {
+    /*
+    console.log(`Name:\t\t${location.name}`);
+    const from = getCategories(location.categories);
+    console.log(`Original:\t${from}`);
+    const matched = matchCategories(from);
+    console.log(`Matched:\t${matched}`);
+    const newCats = renameCategories(matched);
+    console.log(`New names:\t${newCats}`);
+    const weights = getWeights(newCats);
+    console.log(`Weights:`);
+    console.log(weights);
+    const heaviest = getHeaviest(weights);
+    console.log(`Main Category:\t${heaviest}`);
+    console.log();
+    */
+
+    console.log(`Name:\t\t${location.name}`);
+    console.log(`Categories:\t${location.categories}`);
+    const winner = pipeline(getCategories, matchCategories, renameCategories, getWeights, getHeaviest)(location.categories);
+    console.log(`Main:\t\t${winner}\n`);
+  }
+})();
